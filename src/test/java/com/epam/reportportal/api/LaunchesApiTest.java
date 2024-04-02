87a70ffdc7ffe1226f3bcb28c99f6197a46a2cb7
Daria Rudenko <4r.daria@gmail.com>
2024-04-02 11:37:03 +0200

    Change testRunner to TestNG

--- src/test/java/com/epam/reportportal/api/LaunchesApiTest.java
+++ src/test/java/com/epam/reportportal/api/LaunchesApiTest.java
@@ -2,9 +2,10 @@ package com.epam.reportportal.api;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertTrue;
+
 
 public class LaunchesApiTest {
 
--- src/test/java/com/epam/reportportal/ui/LaunchesPageTest.java
+++ src/test/java/com/epam/reportportal/ui/LaunchesPageTest.java
@@ -1,11 +1,10 @@
 package com.epam.reportportal.ui;
 
-import com.epam.reportportal.TestRunner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertTrue;
 
 public class LaunchesPageTest {
 
