377d75b6e88aa20b4472917243e03650bfffe0ea
Daria Rudenko <4r.daria@gmail.com>
2024-04-02 22:46:05 +0200

    ADd real data

--- src/main/java/com/epam/reportportal/services/Login.java
+++ src/main/java/com/epam/reportportal/services/Login.java
@@ -1,5 +1,54 @@
 package com.epam.reportportal.services;
 
+import com.epam.reportportal.model.user.User;
+import com.epam.reportportal.pages.launches.LaunchesPage;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.springframework.stereotype.Component;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+
+@Component
+@NoArgsConstructor
 public class Login {
-        //prepare and perform user login
+    private static final Logger logger = LogManager.getRootLogger();
+    private static final String LOGIN_PAGE_URL = baseUrl + "/ui/#login";
+
+    private static void setInputLogin(String username) {
+        $(By.name("login")).setValue(username);
+    }
+
+    private static void setInputPassword(String password) {
+        $(By.name("password")).setValue(password);
+    }
+
+    private static void clickLoginButton() {
+        $x("//button[contains(text(), 'Login')]").click();
+    }
+
+    public static Login openLoginPage()
+    {
+        open(LOGIN_PAGE_URL);
+        try {
+            open(LOGIN_PAGE_URL);
+            logger.info("Login page opened successfully");
+        } catch (Exception e) {
+            logger.error("Failed to open login page: " + e.getMessage());
+        }
+        return new Login();
+    }
+
+    public static LaunchesPage login(User user)
+    {
+        setInputLogin(user.getUsername());
+        setInputPassword(user.getPassword());
+        clickLoginButton();
+        logger.info("Login performed");
+        return new LaunchesPage();
+    }
 }
+
--- src/main/java/com/epam/reportportal/services/UserCreator.java
+++ src/main/java/com/epam/reportportal/services/UserCreator.java
@@ -1,6 +1,21 @@
 package com.epam.reportportal.services;
 
+import com.epam.reportportal.model.user.User;
+
+import static com.epam.reportportal.utils.configuration.PropertyReader.getProperty;
+
 public class UserCreator {
-    //create user using configuration
+    public static final String ADMIN_USER_NAME = "superadmin.user.name";
+    public static final String ADMIN_USER_PASSWORD = "superadmin.user.password";
+    public static final String DEFAULT_USER_NAME = "default.user.name";
+    public static final String DEFAULT_USER_PASSWORD = "default.user.password";
+
+    public static User adminUser() {
+        return new User(getProperty(ADMIN_USER_NAME), getProperty(ADMIN_USER_PASSWORD));
+    }
+
+    public static User defaultUser() {
+        return new User(getProperty(DEFAULT_USER_NAME), getProperty(DEFAULT_USER_PASSWORD));
+    }
 }
 
