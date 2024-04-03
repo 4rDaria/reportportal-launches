9583ccd8a16b68e5ccc5a011818663835936b853
Daria Rudenko <4r.daria@gmail.com>
2024-04-03 13:13:24 +0200

    Update tests

--- src/test/java/com/epam/reportportal/ui/LaunchesPageTest.java
+++ src/test/java/com/epam/reportportal/ui/LaunchesPageTest.java
@@ -21,7 +21,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
