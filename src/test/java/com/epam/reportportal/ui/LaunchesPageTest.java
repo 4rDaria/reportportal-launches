4e449fdfe9402857504cbf3b161fb2c329c2c1f0
Daria Rudenko <4r.daria@gmail.com>
2024-04-03 06:57:54 +0200

    Add test data storing utility

--- src/test/java/com/epam/reportportal/ui/LaunchesPageTest.java
+++ src/test/java/com/epam/reportportal/ui/LaunchesPageTest.java
@@ -1,30 +1,33 @@
 package com.epam.reportportal.ui;
 
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.back;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static com.epam.reportportal.constants.Constants.*;
+import static com.epam.reportportal.services.Login.login;
+import static com.epam.reportportal.services.Login.openLoginPage;
+import static org.testng.Assert.assertTrue;
+
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import com.epam.reportportal.model.user.User;
 import com.epam.reportportal.pages.common.ModalWindow;
 import com.epam.reportportal.pages.launches.ActionMenu;
 import com.epam.reportportal.pages.launches.GridRow;
 import com.epam.reportportal.pages.launches.LaunchesPage;
 import com.epam.reportportal.services.UserCreator;
+import com.epam.reportportal.utils.TestDataUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.JavascriptExecutor;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.codeborne.selenide.Configuration.baseUrl;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static com.epam.reportportal.constants.Constants.PROJECT;
-import static com.epam.reportportal.services.Login.login;
-import static com.epam.reportportal.services.Login.openLoginPage;
-import static org.testng.Assert.assertTrue;
 
 public class LaunchesPageTest {
 
@@ -32,7 +35,7 @@ public class LaunchesPageTest {
 
     private static LaunchesPage launchesPage;
 
-    @BeforeMethod
+    @BeforeClass
     public void setUp() {
         User testUser = UserCreator.adminUser();
         openLoginPage();
@@ -52,22 +55,7 @@ public class LaunchesPageTest {
         assertTrue(true);
     }
 
-    @DataProvider(name="category")
-    public Object[][] tData() {
-        return new Object[][]
-                {TOTAL, TOTAL_COUNT_CELL_CSS),
-                Arguments.of(PASSED, PASSED_COUNT_CELL_CSS),
-                Arguments.of(FAILED, FAILED_COUNT_CELL_CSS),
-                Arguments.of(SKIPPED, SKIPPED_COUNT_CELL_CSS),
-                Arguments.of(PRODUCT_BUG, PRODUCT_BUG_COUNT_CELL_CSS),
-                Arguments.of(AUTO_BUG, AUTO_BUG_COUNT_CELL_CSS),
-                Arguments.of(SYSTEM_ISSUE, SYSTEM_ISSUE_COUNT_CELL_CSS),
-                Arguments.of(TO_INVESTIGATE, TO_INVESTIGATE_COUNT_CELL_CSS)
-        }
-    }
-
-    @ParameterizedTest(name = "{index} => launch contains tests count data for ''{0}''")
-    @MethodSource("category")
+    @Test(dataProvider = "category", dataProviderClass = TestDataUtils.class)
     public void eachLaunchesContainsAllTestCountData(String category, String css) {
         ElementsCollection gridRowElements = launchesPage.gridRowElements();
 
@@ -81,18 +69,7 @@ public class LaunchesPageTest {
         }
     }
 
-    private static Stream<Arguments> launchesToCompare() {
-        return Stream.of(
-                Arguments.of("two in a row", asList(1, 2)),
-                Arguments.of("three in a row", asList(1, 2, 3)),
-                Arguments.of("two at any order", asList(2, 3)),
-                Arguments.of("three in any order", asList(1, 2, 4)),
-                Arguments.of("four in a row", asList(1, 2, 3, 4))
-        );
-    }
-
-    @ParameterizedTest(name = "{index} => user is able select ''{0}'' launches and compare")
-    @MethodSource("launchesToCompare")
+    @Test(dataProvider = "launchesToCompare", dataProviderClass = TestDataUtils.class)
     public void userIsAbleToSelectSeveralLaunchesAndCompareThem(String testCase, List<Integer> launchesToCompare) {
         ElementsCollection gridRowElements = launchesPage.gridRowElements();
 
@@ -102,9 +79,7 @@ public class LaunchesPageTest {
         //select launches
         toggleSelection(numberLaunchesToCompare);
 
-        ActionMenu actionMenu = launchesPage.openActionMenu();
-        ModalWindow compareModal = actionMenu.compareLaunchesModal();
-        compareModal.buttonWithText("Cancel").click();
+        openAndCloseActionMenu();
 
         //unselect launches
         toggleSelection(numberLaunchesToCompare);
@@ -112,12 +87,18 @@ public class LaunchesPageTest {
         sleep(1000);
     }
 
-    private void toggleSelection(List<GridRow> numberLaunchesToCompare){
-        JavascriptExecutor jse = (JavascriptExecutor)getWebDriver();
+    private void toggleSelection(List<GridRow> numberLaunchesToCompare) {
+        JavascriptExecutor jse = (JavascriptExecutor) getWebDriver();
 
         for (GridRow launch : numberLaunchesToCompare) {
             jse.executeScript("arguments[0].click();", launch.checkbox());
         }
+    }
+
+    private void openAndCloseActionMenu(){
+        ActionMenu actionMenu = launchesPage.openActionMenu();
+        ModalWindow compareModal = actionMenu.compareLaunchesModal();
+        compareModal.buttonWithText("Cancel").click();
     }
 
     @Test
@@ -125,19 +106,8 @@ public class LaunchesPageTest {
         assertTrue(true);
     }
 
-    private static Stream<Arguments> donutElement() {
-        return Stream.of(
-                Arguments.of(PRODUCT_BUG, PRODUCT_BUG_DONUT_IDENTIFICATOR),
-                Arguments.of(AUTO_BUG, AUTO_BUG_DONUT_IDENTIFICATOR),
-                Arguments.of( SYSTEM_ISSUE, SYSTEM_ISSUE_DONUT_IDENTIFICATOR),
-                Arguments.of(TO_INVESTIGATE, TO_INVESTIGATE_DONUT_IDENTIFICATOR)
-        );
-    }
-
-    @ParameterizedTest(name = "{index} => user is able to move to appropriate launch clicking ''{0}''")
-    @MethodSource("donutElement")
-    public void userIsAbleToMoveToAppropriateLaunchViaDonutElement(String category, String elementIdentificator)
-            throws URISyntaxException {
+    @Test(dataProvider = "donutElement", dataProviderClass = TestDataUtils.class)
+    public void userIsAbleToMoveToAppropriateLaunchViaDonutElement(String category, String elementIdentificator) {
         ElementsCollection gridRowElements = launchesPage.gridRowElements();
         GridRow launch = new GridRow(gridRowElements.first());
         String launchId = launch.launchId();
@@ -157,19 +127,10 @@ public class LaunchesPageTest {
         }
     }
 
-    private static Stream<Arguments> countElementWithParam() {
-        return Stream.of(
-                Arguments.of(TOTAL, TOTAL_COUNT_CELL_CSS, "PASSED%252CFAILED%252CSKIPPED%252CINTERRUPTED"),
-                Arguments.of(PASSED, PASSED_COUNT_CELL_CSS, "PASSED"),
-                Arguments.of(FAILED, FAILED_COUNT_CELL_CSS, "FAILED%252CINTERRUPTED"),
-                Arguments.of(SKIPPED, SKIPPED_COUNT_CELL_CSS, "SKIPPED")
-        );
-    }
-
-    @ParameterizedTest(name = "{index} => user is able to move to appropriate launch clicking ''{0}''")
-    @MethodSource("countElementWithParam")
-    public void userIsAbleToMoveToAppropriateLaunchClickingCountElement(String category, String elementIdentificator, String param)
-            throws URISyntaxException {
+    @Test(dataProvider = "countElementWithParam", dataProviderClass = TestDataUtils.class)
+    public void userIsAbleToMoveToAppropriateLaunchClickingCountElement(String category,
+                                                                        String elementIdentificator,
+                                                                        String param) {
         ElementsCollection gridRowElements = launchesPage.gridRowElements();
         GridRow launch = new GridRow(gridRowElements.first());
         String launchId = launch.launchId();
@@ -189,5 +150,4 @@ public class LaunchesPageTest {
             back();
         }
     }
-
 }
