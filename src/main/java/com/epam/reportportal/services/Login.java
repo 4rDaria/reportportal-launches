0bd7e854a7a6707a29d783960f18decb09293eed
Daria Rudenko <4r.daria@gmail.com>
2024-04-17 20:33:04 +0200

    Update files

--- src/main/java/com/epam/reportportal/services/Login.java
+++ src/main/java/com/epam/reportportal/services/Login.java
@@ -1,5 +1,52 @@
 package com.epam.reportportal.services;
 
+import com.epam.reportportal.model.user.User;
+import com.epam.reportportal.pages.launches.LaunchesPage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+import static com.epam.reportportal.utils.configuration.EnvironmentConfiguration.baseUrlForCurrentEnv;
+import static com.epam.reportportal.utils.configuration.EnvironmentConfiguration.projectNameForCurrentEnv;
+
 public class Login {
-        //prepare and perform user login
+    private static final Logger LOGGER = LogManager.getRootLogger();
+    public static final String BASE_URL = baseUrlForCurrentEnv();
+    public static final String PROJECT = projectNameForCurrentEnv();
+    private static final String LOGIN_PAGE_URL = BASE_URL + "/ui/#login";
+    private static void setInputLogin(String username) {
+        $(By.name("login")).setValue(username);
+    }
+
+    private static void setInputPassword(String password) {
+        $(By.name("password")).setValue(password);
+    }
+
+    private static void clickLoginButton() {
+        $x("//button[contains(text(), 'Login')]").click();
+    }
+
+    public static Login openLoginPage()
+    {
+        open(LOGIN_PAGE_URL);
+        try {
+            open(LOGIN_PAGE_URL);
+            LOGGER.info("Login page opened successfully");
+        } catch (Exception e) {
+            LOGGER.error("Failed to open login page: " + e.getMessage());
+        }
+        return new Login();
+    }
+
+    public static LaunchesPage login(User user)
+    {
+        setInputLogin(user.getUsername());
+        setInputPassword(user.getPassword());
+        clickLoginButton();
+        LOGGER.info("Login performed");
+        open(BASE_URL + "/ui/#" + PROJECT + "/launches/all");
+        return new LaunchesPage();
+    }
 }
--- src/test/java/com/epam/reportportal/ui/LaunchesPageTest.java
+++ src/test/java/com/epam/reportportal/ui/LaunchesPageTest.java
@@ -1,14 +1,45 @@
 package com.epam.reportportal.ui;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import com.epam.reportportal.model.user.User;
+import com.epam.reportportal.pages.launches.GridRow;
+import com.epam.reportportal.pages.launches.LaunchesPage;
+import com.epam.reportportal.services.UserCreator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static com.epam.reportportal.constants.Constants.*;
+import static com.epam.reportportal.services.Login.login;
+import static com.epam.reportportal.services.Login.openLoginPage;
+import static com.epam.reportportal.utils.configuration.EnvironmentConfiguration.baseUrlForCurrentEnv;
+import static com.epam.reportportal.utils.configuration.EnvironmentConfiguration.projectNameForCurrentEnv;
 import static org.junit.Assert.assertTrue;
 
 public class LaunchesPageTest {
 
-    private static final Logger logger = LogManager.getRootLogger();
+    private static final Logger LOGGER = LogManager.getRootLogger();
+    public static final String BASE_URL = baseUrlForCurrentEnv();
+    public static final String PROJECT = projectNameForCurrentEnv();
+    private static LaunchesPage launchesPage;
+
+    @BeforeEach
+    public void setUp() {
+        User testUser = UserCreator.withCredentialsFromProperty();
+        openLoginPage();
+        getWebDriver().manage().window().maximize();
+        launchesPage = login(testUser);
+        sleep(500);
+    }
 
     @Test
     public void launchesListSortedByMostRecentByDefault() {
