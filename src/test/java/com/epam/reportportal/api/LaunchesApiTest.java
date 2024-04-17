55562e38bf337eb5630a7eeebe4e62fd7a783738
Daria Rudenko <4r.daria@gmail.com>
2024-04-17 20:18:06 +0200

    Add JUnit5 TestRunner

--- src/test/java/com/epam/reportportal/api/LaunchesApiTest.java
+++ src/test/java/com/epam/reportportal/api/LaunchesApiTest.java
@@ -2,9 +2,9 @@ package com.epam.reportportal.api;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.testng.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;
 
 
 public class LaunchesApiTest {
--- src/test/java/com/epam/reportportal/ui/LaunchesPageTest.java
+++ src/test/java/com/epam/reportportal/ui/LaunchesPageTest.java
@@ -2,9 +2,9 @@ package com.epam.reportportal.ui;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;
 
 public class LaunchesPageTest {
 
